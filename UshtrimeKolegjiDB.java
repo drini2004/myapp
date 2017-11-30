9a3e1672097960d783870289d058ce0096b7f290
drini2004 <33898619+drini2004@users.noreply.github.com>
2017-11-30 19:09:34 +0100

    Add files via upload

---
+++ UshtrimeKolegjiDB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ushtrimekolegjidb;
+
+import UI.FormLogin;
+import UI.FormStudentetList;
+import UI.FormStudenti;
+
+/**
+ *
+ * @author MensurOsmani
+ */
+public class UshtrimeKolegjiDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormLogin loginForm = new FormLogin();
+        loginForm.setVisible(true);
+    }
+    
+}
